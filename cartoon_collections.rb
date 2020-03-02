aab8b3a969aac2e19d9cdac330dea8cbf5e0c7fc
Bryant Omoregie <unclebryant@gmail.com>
2020-03-02 02:09:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,6 @@ array.any? {|x| x.length > 4}
 end
 
 def find_the_cheese(array)
-  
   cheese_types = ["cheddar", "gouda", "camembert"]
-  
+  array.find { |cheese| cheese_types.include?(cheese) }
 end
